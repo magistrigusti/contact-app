2862f5a5b1201f7e4c9d01b8984b89ee320ee34b
Magistry <Magistrigusti@yandex.ru>
2023-07-18 05:18:42 +0300

    0.0.4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 
 import Header from '../Header/Header';
 import AppRoutes from '../Routes/Routes';
 import Sidebar from '../Sidebar/Sidebar';
 import Footer from '../Footer/Footer';
+import { getCategories } from '../../features/categories/categoriesSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch])
   return (
     <div className="app">
       <Header />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 import styles from '../../styles/Sidebar.module.css';
 
 const Sidebar = () => {
+  const { list } = useSelector(({ categories }) => categories);
+  console.log('list', list);
+
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}>CATEGORIES</div>
